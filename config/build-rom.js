d938e4e756938491d3bf680e5119b743b625355f
krlosMata <krlosmata@gmail.com>
2022-10-24 09:01:27 +0200

    working recursive

--- config/build-rom.js
+++ config/build-rom.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
-const version = require('../package').version;
+const { version } = require('../package.json');
 
-const argv = require('yargs')
+// eslint-disable-next-line import/order
+const { argv } = require('yargs')
     .version(version)
     .usage('node build-rom.js --output <rom.json> --steps <steps>')
     .alias('i', 'input')
     .alias('o', 'output')
-    .alias('s', 'steps')
-    .argv;
+    .alias('s', 'steps');
 
-async function main(){
+async function main() {
     // setup input file
     const defaultInputPath = 'main/main.zkasm';
     const pathInput = (typeof argv.input !== 'undefined') ? argv.input : defaultInputPath;
@@ -24,12 +25,12 @@ async function main(){
     const defaultSteps = 'no-change';
     const steps = (typeof argv.steps !== 'undefined') ? Number(argv.steps) : defaultSteps;
 
-    if (steps !== defaultSteps){
+    if (steps !== defaultSteps) {
         // replace steps in constants.zkasm
-        const pathRomConstFile = path.join(__dirname, `../main/constants.zkasm`);
+        const pathRomConstFile = path.join(__dirname, '../main/constants.zkasm');
         const oldContentConstFile = fs.readFileSync(pathRomConstFile, 'utf8');
         // line 'CONST %MAX_CNT_STEPS'
-        const oldContentSteps = oldContentConstFile.split("\n").filter(elem => elem.includes('CONST %MAX_CNT_STEPS'))[0];
+        const oldContentSteps = oldContentConstFile.split('\n').filter((elem) => elem.includes('CONST %MAX_CNT_STEPS'))[0];
         // extract steps number
         const oldSplitLines = oldContentSteps.split('**');
 
@@ -53,8 +54,8 @@ async function main(){
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
