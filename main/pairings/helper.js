2f29c4a8f304dcd12d16c46a3692c856fd3872c7
hecmas <lecter1729@gmail.com>
2023-04-26 17:26:08 +0200

    Keep working. I start to see the light at the end of the tunnel

--- main/pairings/helper.js
+++ main/pairings/helper.js
@@ -1,3 +1,8 @@
+// const { F1Field } = require("ffjavascript");
+
+// const FpBN254 = new F1Field(21888242871839275222246405745257275088696311157297823662689037894645226208583n);
+// const FrBN254 = new F1Field(21888242871839275222246405745257275088548364400416034343698204186575808495617n);
+
 module.exports = class myHelper {
     setup (props) {
         for(const name in props) {
@@ -40,5 +45,54 @@ module.exports = class myHelper {
             console.log(`${param.regName}: [${ctx[param.regName].map(x => "0x"+x.toString(16).padStart(16, '0')).join(',')}]`);
         }
         return 0n;
+    }
+    eval_xAddPointTwistBN254(ctx, tag) {
+        return eval_AddPointTwistBN254(ctx, tag, false)[0];
+    }
+    
+    eval_yAddPointTwistBN254(ctx, tag) {
+        return eval_AddPointTwistBN254(ctx, tag, false)[1];
+    }
+    
+    eval_xDblPointTwistBN254(ctx, tag) {
+        return eval_AddPointTwistBN254(ctx, tag, true)[0];
+    }
+    
+    eval_yDblPointTwistBN254(ctx, tag) {
+        return eval_AddPointTwistBN254(ctx, tag, true)[1];
+    }
+    
+    eval_AddPointTwistBN254(ctx, tag, dbl)
+    {
+        const x11 = evalCommand(ctx, tag.params[0]);
+        const x12 = evalCommand(ctx, tag.params[1]);
+        const y11 = evalCommand(ctx, tag.params[2]);
+        const y12 = evalCommand(ctx, tag.params[3]);
+        const x21 = evalCommand(ctx, tag.params[dbl ? 0 : 4]);
+        const x22 = evalCommand(ctx, tag.params[dbl ? 1 : 5]);
+        const y21 = evalCommand(ctx, tag.params[dbl ? 2 : 6]);
+        const y22 = evalCommand(ctx, tag.params[dbl ? 3 : 7]);
+    
+        let s;
+        if (dbl) {
+            // Division by zero must be managed by ROM before call ARITH
+            const divisor = ctx.Fec.add(y1, y1)
+            if (ctx.Fec.isZero(divisor)) {
+                throw new Error(`Invalid AddPointTwistBN254 (divisionByZero) ${ctx.sourceRef}`);
+            }
+            s = ctx.Fec.div(ctx.Fec.mul(3n, ctx.Fec.mul(x1, x1)), divisor);
+        }
+        else {
+            const deltaX = ctx.Fec.sub(x2, x1)
+            if (ctx.Fec.isZero(deltaX)) {
+                throw new Error(`Invalid AddPointTwistBN254 (divisionByZero) ${ctx.sourceRef}`);
+            }
+            s = ctx.Fec.div(ctx.Fec.sub(y2, y1), deltaX );
+        }
+    
+        const x3 = ctx.Fec.sub(ctx.Fec.mul(s, s), ctx.Fec.add(x1, x2));
+        const y3 = ctx.Fec.sub(ctx.Fec.mul(s, ctx.Fec.sub(x1,x3)), y1);
+    
+        return [x3, y3];
     }
 }
