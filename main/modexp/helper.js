18eedd0fa1f599d7deab808daea8419864816bf7
Héctor Masip <hmasip@polygon.technology>
2023-07-17 12:30:28 +0200

    Array addition included

--- main/modexp/helper.js
+++ main/modexp/helper.js
@@ -36,38 +36,8 @@ module.exports = class myHelper {
         a.length = i + 1;
     }
 
-    // Assumes a.length >= b.length
-    _array_add(a, b) {
-        const alen = a.length;
-        const blen = b.length;
-        let result = new Array(alen);
-        let sum = 0n;
-        let carry = 0n;
-        for (let i = 0; i < blen; i++) {
-            sum = a[i] + b[i] + carry;
-            carry = sum >= this.base ? 1n : 0n;
-            result[i] = sum - carry * this.base;
-        }
-        for (let i = blen; i < alen; i++) {
-            sum = a[i] + carry;
-            carry = sum == this.base ? 1n : 0n; // the past carry is at most 1n
-            result[i] = sum - carry * this.base;
-        }
-
-        if (carry === 1n) {
-            result.push(carry);
-        }
-        return result;
-    }
-    array_add(a, b) {
-        if (a.length < b.length) {
-            return _array_add(b, a);
-        }
-        return _array_add(a, b);
-    }
-
     // Assumes a >= b
-    _array_sub(a, b) {
+    _MP_sub(a, b) {
         const alen = a.length;
         const blen = b.length;
         let result = new Array(alen);
@@ -95,12 +65,12 @@ module.exports = class myHelper {
         this.trim(result);
         return result;
     }
-    array_sub(a, b) {
+    MP_sub(a, b) {
         let result;
         if (this.compare(a, b) >= 0) {
-            result = this._array_sub(a, b);
+            result = this._MP_sub(a, b);
         } else {
-            result = this._array_sub(b, a);
+            result = this._MP_sub(b, a);
             result[result.length - 1] = -result[result.length - 1];
         }
         if (result.length === 0) {
@@ -109,7 +79,7 @@ module.exports = class myHelper {
         return result;
     }
 
-    array_long_mul(a, b) {
+    MP_long_mul(a, b) {
         const alen = a.length;
         const blen = b.length;
         const len = alen + blen;
@@ -131,7 +101,7 @@ module.exports = class myHelper {
         this.trim(result);
         return result;
     }
-    array_short_mul(a, b) {
+    MP_short_mul(a, b) {
         const alen = a.length;
         const len = alen;
         const result = new Array(len).fill(0n);
@@ -155,12 +125,12 @@ module.exports = class myHelper {
         let bm = b[b.length - 1];
         let shift = 1n; // shift cannot be larger than log2(base) - 1
         while (bm < this.base / 2n) {
-            b = this.array_short_mul(b, 2n); // left-shift b by 2
+            b = this.MP_short_mul(b, 2n); // left-shift b by 2
             bm = b[b.length - 1];
             shift *= 2n;
         }
 
-        a = this.array_short_mul(a, shift); // left-shift a by 2^shift
+        a = this.MP_short_mul(a, shift); // left-shift a by 2^shift
         return [a, b, shift];
     }
 
@@ -190,7 +160,7 @@ module.exports = class myHelper {
             }
 
             if (an[n - 1] < bm) {
-                qn = this.array_short_div(aguess, bm)[0][0]; // this is always a single digit
+                qn = this.MP_short_div(aguess, bm)[0][0]; // this is always a single digit
             } else if (an[n - 1] === bm) {
                 if (b_l < n) {
                     qn = this.base - 1n;
@@ -201,24 +171,26 @@ module.exports = class myHelper {
                 qn = 1n;
             }
 
-            test = this.array_short_mul(b, qn);
+            test = this.MP_short_mul(b, qn);
             while (this.compare(test, an) === 1) {
                 // maximum 2 iterations
                 qn--;
-                test = this.array_sub(test, b);
+                test = this.MP_sub(test, b);
             }
 
             quotient.unshift(qn);
-            remainder = this.array_sub(an, test);
+            remainder = this.MP_sub(an, test);
             an = remainder;
             if (a_l === 0) break;
             an.unshift(a[--a_l]);
         }
-        remainder = this.array_short_div(remainder, shift)[0];
+        remainder = this.MP_short_div(remainder, shift)[0];
+        this.trim(quotient);
+        this.trim(remainder);
         return [quotient, remainder];
     }
 
-    array_short_div(a, b) {
+    MP_short_div(a, b) {
         let a_l = a.length;
         let quotient = [];
         let remainder = 0n;
@@ -267,5 +239,13 @@ module.exports = class myHelper {
         const pos = Number(this.evalCommand(ctx, tag.params[0]));
         const remi = ctx.remainder[pos];
         return remi;
+    }
+    
+    eval_receiveLenQuotient(ctx) {
+        return ctx.quotient.length;
+    }
+
+    eval_receiveLenRemainder(ctx) {
+        return ctx.remainder.length;
     }
 };
